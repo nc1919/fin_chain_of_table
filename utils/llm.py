80245324828a1ea11971f2a82562e0dbeda576ed
Neel Chaudhari <105801263+nc1919@users.noreply.github.com>
2024-10-14 16:47:26 +0100

    Update llm.py

--- utils/llm.py
+++ utils/llm.py
@@ -1,97 +1,68 @@
-# Copyright 2024 The Chain-of-Table authors
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     https://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-
-import openai
+import requests
 import time
 import numpy as np
 
-
-class ChatGPT:
+class GeminiAPI:
     def __init__(self, model_name, key):
         self.model_name = model_name
         self.key = key
+        self.api_url = f"https://generativelanguage.googleapis.com/v1beta/models/{self.model_name}:generateContent?key={self.key}"
 
-    def get_model_options(
-        self,
-        temperature=0,
-        per_example_max_decode_steps=150,
-        per_example_top_p=1,
-        n_sample=1,
-    ):
-        return dict(
-            temperature=temperature,
-            n=n_sample,
-            top_p=per_example_top_p,
-            max_tokens=per_example_max_decode_steps,
-        )
+    def get_model_options(self, temperature=0, max_decode_steps=150, n_sample=1):
+        return {
+            "temperature": temperature,
+            "max_decode_steps": max_decode_steps,
+            "n_sample": n_sample,
+        }
 
-    def generate_plus_with_score(self, prompt, options=None, end_str=None):
+    def generate_with_score(self, prompt, options=None):
         if options is None:
             options = self.get_model_options()
-        messages = [
-            {
-                "role": "system",
-                "content": "I will give you some examples, you need to follow the examples and complete the text, and no other content.",
-            },
-            {"role": "user", "content": prompt},
-        ]
-        gpt_responses = None
+        
+        headers = {
+            'Content-Type': 'application/json',
+        }
+
+        data = {
+            "contents": [{"parts": [{"text": prompt}]}],
+            "temperature": options["temperature"],
+            "n_sample": options["n_sample"],
+        }
+
+        response = None
         retry_num = 0
         retry_limit = 2
         error = None
-        while gpt_responses is None:
+        while response is None:
             try:
-                gpt_responses = openai.ChatCompletion.create(
-                    model=self.model_name,
-                    messages=messages,
-                    stop=end_str,
-                    api_key=self.key,
-                    **options
-                )
+                response = requests.post(self.api_url, headers=headers, json=data)
+                response.raise_for_status()
                 error = None
-            except Exception as e:
+            except requests.exceptions.RequestException as e:
                 print(str(e), flush=True)
                 error = str(e)
-                if "This model's maximum context length is" in str(e):
-                    print(e, flush=True)
-                    gpt_responses = {
-                        "choices": [{"message": {"content": "PLACEHOLDER"}}]
-                    }
-                elif retry_num > retry_limit:
-                    error = "too many retry times"
-                    gpt_responses = {
-                        "choices": [{"message": {"content": "PLACEHOLDER"}}]
-                    }
+                if retry_num > retry_limit:
+                    error = "too many retry attempts"
+                    return [{"text": "PLACEHOLDER"}]
                 else:
                     time.sleep(60)
                 retry_num += 1
+        
         if error:
             raise Exception(error)
+        
+        response_json = response.json()
         results = []
-        for i, res in enumerate(gpt_responses["choices"]):
-            text = res["message"]["content"]
-            fake_conf = (len(gpt_responses["choices"]) - i) / len(
-                gpt_responses["choices"]
-            )
+        for i, res in enumerate(response_json.get("contents", [])):
+            text = res["parts"][0]["text"]
+            fake_conf = (len(response_json["contents"]) - i) / len(response_json["contents"])
             results.append((text, np.log(fake_conf)))
-
+        
         return results
 
-    def generate(self, prompt, options=None, end_str=None):
+    def generate(self, prompt, options=None):
         if options is None:
             options = self.get_model_options()
-        options["n"] = 1
-        result = self.generate_plus_with_score(prompt, options, end_str)[0][0]
+        options["n_sample"] = 1
+        result = self.generate_with_score(prompt, options)[0][0]
         return result
